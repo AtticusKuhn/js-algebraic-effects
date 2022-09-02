4eb33b49c95acff4e0ebbd43c29b64b850887e46
Atticus Kuhn <52258164+AtticusKuhn@users.noreply.github.com>
2022-09-02 21:15:11 +0000

    add comments

--- effects.js
+++ effects.js
@@ -1,18 +1,22 @@
 const {mkHanlder,effectMaker} = require("./lib.js")
 
 const {log, rand,getEnv} = effectMaker();
-
+// an effectful handler recieves
+// the program and actually performs
+// the effects
 const effectfulHandler = mkHanlder({
     "log": (x)=>console.log(x),
     "rand":()=> Math.floor(10*Math.random()),
     "getEnv": (x)=>process.env[x],
 })
-
+// a pure handler mocks the effects
+// and never actually performs any effects
 const pureHandler =  mkHanlder({
     "log": (x)=>x,
     "rand":()=> 1,
 })
-
+// a pure handler for environment variables
+// that takes in a parameter
 const envHandler = (testEnv) => mkHanlder({
     "getEnv" : (x)=>testEnv[x],
 })
--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const {Do} =require("./lib.js")
 const {rand, log, getEnv, pureHandler,effectfulHandler,envHandler} =require( "./effects.js")
-
+// a program to generate 2 random numbers
+// and then print their sum
 const addRandomNumbers = Do(function*() {
     const r1 = yield rand();
     const r2 = yield rand();
     yield log(`${r1}+${r2} is the same as ${r1+r2}`)
     return r1+r2
 })
-
+// a program get the current user from
+// environment variables
 const envProgram = Do(function*(){
-    return (yield getEnv("REPL_OWNER"))
+    return (yield getEnv("USER"))
 })
-console.log(addRandomNumbers)
+// test random number program with
+// a pure handler
 console.log("pure handling is", pureHandler(addRandomNumbers));
+// run the random number program
+// effectfully
 console.log("effectful handling is:",effectfulHandler(addRandomNumbers))
+// run the env program by using 
+// environment variables
 console.log("env", effectfulHandler(envProgram))
-console.log("env pure", envHandler({"REPL_OWNER":"Rohan"})(envProgram))
+// run env progrom purely by passing
+// in an object
+console.log("env pure", envHandler({"USER":"Rohan"})(envProgram))
